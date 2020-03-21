fbb75c097045a8200a1e62fa98842ec792d4bb39
Michael Moghaddam <m.moghaddam385@outlook.com>
2020-03-21 16:58:40 -0700

    Add more websocket message models and add Java samples

--- src/main/kotlin/io/polygon/kotlin/sdk/websocket/PolygonWebSocketMessage.kt
+++ src/main/kotlin/io/polygon/kotlin/sdk/websocket/PolygonWebSocketMessage.kt
@@ -8,12 +8,87 @@ import kotlinx.serialization.Serializable
  */
 sealed class PolygonWebSocketMessage {
 
+    data class RawMessage(val data: ByteArray) : PolygonWebSocketMessage()
+
     @Serializable
     data class StatusMessage(
         val ev: String? = null,
         val status: String? = null,
         val message: String? = null
     ) : PolygonWebSocketMessage()
+
+    sealed class StocksMessage : PolygonWebSocketMessage() {
+
+        @Serializable
+        data class Trade(
+            @SerialName("ev") val eventType: String? = null,
+            @SerialName("sym") val ticker: String? = null,
+            @SerialName("x") val exchangeId: Long? = null,
+            @SerialName("i") val tradeId: String? = null,
+            @SerialName("z") val tape: String? = null,
+            @SerialName("p") val price: Double? = null,
+            @SerialName("s") val size: Double? = null,
+            @SerialName("c") val conditions: List<Int> = emptyList(),
+            @SerialName("t") val timestampMillis: Long? = null
+        ) : StocksMessage()
+
+        @Serializable
+        data class Quote(
+            @SerialName("ev") val eventType: String? = null,
+            @SerialName("sym") val ticker: String? = null,
+            @SerialName("bx") val bidExchangeId: Long? = null,
+            @SerialName("bp") val bidPrice: Double? = null,
+            @SerialName("bs") val bidSize: Double? = null,
+            @SerialName("ax") val askExchangeId: Long? = null,
+            @SerialName("ap") val askPrice: Double? = null,
+            @SerialName("as") val askSize: Double? = null,
+            @SerialName("c") val condition: Int? = null,
+            @SerialName("t") val timestampMillis: Long? = null
+        ) : StocksMessage()
+
+        @Serializable
+        data class Aggregate(
+            @SerialName("ev") val eventType: String? = null,
+            @SerialName("sym") val ticker: String? = null,
+            @SerialName("v") val volume: Double? = null,
+            @SerialName("av") val accumulatedVolume: Double? = null,
+            @SerialName("op") val officialOpenPrice: Double? = null,
+            @SerialName("vw") val volumeWeightedAveragePrice: Double? = null,
+            @SerialName("o") val openPrice: Double? = null,
+            @SerialName("c") val closePrice: Double? = null,
+            @SerialName("h") val highPrice: Double? = null,
+            @SerialName("l") val lowPrice: Double? = null,
+            @SerialName("a") val averagePrice: Double? = null,
+            @SerialName("s") val startTimestampMillis: Long? = null,
+            @SerialName("e") val endTimestampMillis: Long? = null
+        ) : StocksMessage()
+    }
+
+    sealed class ForexMessage : PolygonWebSocketMessage() {
+
+        @Serializable
+        data class Quote(
+            @SerialName("ev") val eventType: String? = null,
+            @SerialName("p") val currencyPair: String? = null,
+            @SerialName("x") val exchangeId: Long? = null,
+            @SerialName("a") val askPrice: Double? = null,
+            @SerialName("b") val bidPrice: Double? = null,
+            @SerialName("t") val timestampMillis: Long? = null
+        ) : ForexMessage()
+
+        @Serializable
+        data class Aggregate(
+            @SerialName("ev") val eventType: String? = null,
+            @SerialName("pair") val currencyPair: String? = null,
+            @SerialName("o") val openPrice: Double? = null,
+            @SerialName("c") val closePrice: Double? = null,
+            @SerialName("h") val highPrice: Double? = null,
+            @SerialName("l") val lowPrice: Double? = null,
+            @SerialName("v") val quoteInAgg: Int? = null,
+            @SerialName("s") val startTimestampMillis: Long? = null,
+            @SerialName("e") val endTimestampMillis: Long? = null
+        ) : ForexMessage()
+    }
 
     sealed class CryptoMessage : PolygonWebSocketMessage() {
 
@@ -44,9 +119,5 @@ sealed class PolygonWebSocketMessage {
             @SerialName("x") val exchangeId: Long? = null,
             @SerialName("r") val receivedAtPolygonTimestamp: Long? = null
         ) : CryptoMessage()
-
     }
-
-    data class RawMessage(val data: ByteArray) : PolygonWebSocketMessage()
-
 }
