0e203bd939fa390f798c192523b48ec1071941e0
Michael Moghaddam <6424916+mmoghaddam385@users.noreply.github.com>
2020-07-11 12:18:03 -0400

    Make http client provider more configurable
    Update sample with expanded config options

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -8,6 +8,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
 
+    // For custom interceptors on defaultOkHttpClientProvider
+    implementation("com.squareup.okhttp3:okhttp:4.7.2")
+
+    // For custom CIO engine on defaultJvmHttpClientProvider
+    implementation("io.ktor:ktor-client-cio:1.3.1")
+
     // For pretty printing data classes
     implementation("com.tylerthrailkill.helpers:pretty-print:2.0.2")
 }
--- src/main/kotlin/io/polygon/kotlin/sdk/HttpClientProvider.kt
+++ src/main/kotlin/io/polygon/kotlin/sdk/HttpClientProvider.kt
@@ -1,6 +1,10 @@
 package io.polygon.kotlin.sdk
 
 import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.HttpClientEngineConfig
+import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
@@ -9,6 +13,7 @@ import io.ktor.http.DEFAULT_PORT
 import io.ktor.http.URLBuilder
 import io.ktor.http.URLProtocol
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 
 interface HttpClientProvider {
     fun buildClient(): HttpClient
@@ -16,9 +21,44 @@ interface HttpClientProvider {
     fun getDefaultRestURLBuilder(): URLBuilder
 }
 
-open class DefaultJvmHttpClientProvider : HttpClientProvider {
+/**
+ * A default [HttpClientProvider] which provides an [HttpClient] powered by the [OkHttp] engine.
+ *
+ * For more details on the interceptors and the difference between an application interceptor and a network interceptor,
+ * see OkHttp's documentation: https://square.github.io/okhttp/interceptors/
+ */
+open class DefaultOkHttpClientProvider
+@JvmOverloads
+constructor(
+    private val applicationInterceptors: List<Interceptor> = emptyList(),
+    private val networkInterceptors: List<Interceptor> = emptyList()
+) : DefaultJvmHttpClientProvider() {
+
+    override fun buildEngine() =
+        OkHttp.create {
+            applicationInterceptors.forEach(::addInterceptor)
+            networkInterceptors.forEach(::addNetworkInterceptor)
+        }
+}
+
+/**
+ * A default [HttpClientProvider] which provides an [HttpClient] powered by a configurable [HttpClientEngine] engine.
+ * For a list of possible engines, see https://ktor.io/clients/http-client/engines.html
+ *
+ * Engine defaults to [OkHttp]
+ *
+ * @see DefaultOkHttpClientProvider
+ */
+open class DefaultJvmHttpClientProvider
+@JvmOverloads
+constructor(
+    private val engine: HttpClientEngine = OkHttp.create()
+) : HttpClientProvider {
+
+    open fun buildEngine(): HttpClientEngine = engine
+
     override fun buildClient() =
-        HttpClient(OkHttp) {
+        HttpClient(buildEngine()) {
             install(WebSockets)
             install(JsonFeature) {
                 serializer = KotlinxSerializer(Json.nonstrict)
