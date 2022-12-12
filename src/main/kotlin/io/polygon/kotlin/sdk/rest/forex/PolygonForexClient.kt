48755f47b12546b41711ff352a593f91411e7a1a
HunterL <6395201+HunterL@users.noreply.github.com>
2022-12-12 12:10:08 -0500

    rest options for forex

--- src/main/kotlin/io/polygon/kotlin/sdk/rest/forex/PolygonForexClient.kt
+++ src/main/kotlin/io/polygon/kotlin/sdk/rest/forex/PolygonForexClient.kt
@@ -3,6 +3,7 @@ package io.polygon.kotlin.sdk.rest.forex
 import io.polygon.kotlin.sdk.ext.coroutineToRestCallback
 import io.polygon.kotlin.sdk.rest.PolygonRestApiCallback
 import io.polygon.kotlin.sdk.rest.PolygonRestClient
+import io.polygon.kotlin.sdk.rest.PolygonRestOption
 import io.polygon.kotlin.sdk.rest.stocks.GainersOrLosersDirection
 import kotlinx.coroutines.runBlocking
 
@@ -19,64 +20,86 @@ internal constructor(internal val polygonClient: PolygonRestClient) {
      * Example for USD/JPY the from would be USD and to would be JPY.
      * The date formatted like 2017-6-22
      *
-     * API Doc: https://polygon.io/docs/#get_v1_historic_forex__from___to___date__anchor
+     * API Doc: https://polygon.io/docs/forex/deprecated/get_v1_historic_forex__from___to___date
      */
-    fun getHistoricTicksBlocking(params: HistoricTicksParameters): HistoricTicksDTO =
-        runBlocking { getHistoricTicks(params) }
-    
+    fun getHistoricTicksBlocking(params: HistoricTicksParameters, vararg opts: PolygonRestOption): HistoricTicksDTO =
+        runBlocking { getHistoricTicks(params, *opts) }
+
     /** See [getHistoricTicksBlocking] */
-    fun getHistoricTicks(params: HistoricTicksParameters, callback: PolygonRestApiCallback<HistoricTicksDTO>) =
-        coroutineToRestCallback(callback, { getHistoricTicks(params) })
-    
+    fun getHistoricTicks(
+        params: HistoricTicksParameters,
+        callback: PolygonRestApiCallback<HistoricTicksDTO>,
+        vararg opts: PolygonRestOption
+    ) = coroutineToRestCallback(callback, { getHistoricTicks(params, *opts) })
+
     /**
      * Convert currencies using the latest market conversion rates.
      * Note than you can convert in both directions. For example USD->CAD or CAD->USD.
      *
-     * API Doc: https://polygon.io/docs/#get_v1_conversion__from___to__anchor
+     * API Doc: https://polygon.io/docs/forex/get_v1_conversion__from___to
      */
-    fun getRealTimeConversionBlocking(params: RealTimeConversionParameters): RealTimeConversionDTO =
-        runBlocking { getRealTimeConversion(params) }
-    
+    fun getRealTimeConversionBlocking(
+        params: RealTimeConversionParameters,
+        vararg opts: PolygonRestOption
+    ): RealTimeConversionDTO = runBlocking { getRealTimeConversion(params, *opts) }
+
     /** See [getRealTimeConversionBlocking] */
-    fun getRealTimeConversion(params: RealTimeConversionParameters, callback: PolygonRestApiCallback<RealTimeConversionDTO>) =
-        coroutineToRestCallback(callback, { getRealTimeConversion(params) })
-    
+    fun getRealTimeConversion(
+        params: RealTimeConversionParameters,
+        callback: PolygonRestApiCallback<RealTimeConversionDTO>,
+        vararg opts: PolygonRestOption
+    ) = coroutineToRestCallback(callback, { getRealTimeConversion(params, *opts) })
+
     /**
      * Get Last Quote Tick for a Currency Pair.
      *
-     * API Doc: https://polygon.io/docs/#get_v1_last_quote_currencies__from___to__anchor
+     * API Doc: https://polygon.io/docs/forex/get_v1_last_quote_currencies__from___to
      */
-    fun getLastQuoteBlocking(fromCurrency: String, toCurrency: String): LastQuoteForexDTO =
-        runBlocking { getLastQuote(fromCurrency, toCurrency) }
-    
+    fun getLastQuoteBlocking(
+        fromCurrency: String,
+        toCurrency: String,
+        vararg opts: PolygonRestOption
+    ): LastQuoteForexDTO = runBlocking { getLastQuote(fromCurrency, toCurrency, *opts) }
+
     /** See [getLastQuoteBlocking] */
-    fun getLastQuote(fromCurrency: String, toCurrency: String, callback: PolygonRestApiCallback<LastQuoteForexDTO>) =
-        coroutineToRestCallback(callback, { getLastQuote(fromCurrency, toCurrency) })
-    
+    fun getLastQuote(
+        fromCurrency: String,
+        toCurrency: String,
+        callback: PolygonRestApiCallback<LastQuoteForexDTO>,
+        vararg opts: PolygonRestOption
+    ) = coroutineToRestCallback(callback, { getLastQuote(fromCurrency, toCurrency, *opts) })
+
     /**
      * Snapshot allows you to see all tickers current minute aggregate, daily aggregate and last trade.
      * As well as previous days aggregate and calculated change for today.
      * The response size is large.
      *
-     * API Doc: https://polygon.io/docs/#get_v2_snapshot_locale_global_markets_forex_tickers_anchor
+     * API Doc: https://polygon.io/docs/forex/get_v2_snapshot_locale_global_markets_forex_tickers
      */
-    fun getSnapshotAllTickersBlocking(): SnapshotForexTickersDTO =
-        runBlocking { getSnapshotAllTickers() }
-    
+    fun getSnapshotAllTickersBlocking(vararg opts: PolygonRestOption): SnapshotForexTickersDTO =
+        runBlocking { getSnapshotAllTickers(*opts) }
+
     /** See [getSnapshotAllTickersBlocking] */
-    fun getSnapshotAllTickers(callback: PolygonRestApiCallback<SnapshotForexTickersDTO>) =
-        coroutineToRestCallback(callback, { getSnapshotAllTickers() })
+    fun getSnapshotAllTickers(
+        callback: PolygonRestApiCallback<SnapshotForexTickersDTO>,
+        vararg opts: PolygonRestOption
+    ) = coroutineToRestCallback(callback, { getSnapshotAllTickers(*opts) })
 
     /**
      * See the current snapshot of the top 20 gainers or losers of the day at the moment.
      *
-     * API Doc: https://polygon.io/docs/#get_v2_snapshot_locale_global_markets_forex__direction__anchor
+     * API Doc: https://polygon.io/docs/forex/get_v2_snapshot_locale_global_markets_forex__direction
      */
-    fun getSnapshotGainersOrLosersBlocking(direction: GainersOrLosersDirection): SnapshotForexTickersDTO =
-        runBlocking { getSnapshotGainersOrLosers(direction) }
-    
+    fun getSnapshotGainersOrLosersBlocking(
+        direction: GainersOrLosersDirection,
+        vararg opts: PolygonRestOption
+    ): SnapshotForexTickersDTO = runBlocking { getSnapshotGainersOrLosers(direction, *opts) }
+
     /** See [getSnapshotGainersOrLosersBlocking] */
-    fun getSnapshotGainersOrLosers(direction: GainersOrLosersDirection, callback: PolygonRestApiCallback<SnapshotForexTickersDTO>) =
-        coroutineToRestCallback(callback, { getSnapshotGainersOrLosers(direction) })
+    fun getSnapshotGainersOrLosers(
+        direction: GainersOrLosersDirection,
+        callback: PolygonRestApiCallback<SnapshotForexTickersDTO>,
+        vararg opts: PolygonRestOption
+    ) = coroutineToRestCallback(callback, { getSnapshotGainersOrLosers(direction, *opts) })
 
 }
