7685d248da9e269a94afc45ffb8ca92a7ed85c8c
Michael Moghaddam <6424916+mmoghaddam385@users.noreply.github.com>
2022-12-16 16:06:18 -0500

    deprecated unsupported reference APIs (#46)
    reference/markets and reference/locales

--- src/main/kotlin/io/polygon/kotlin/sdk/rest/reference/Markets.kt
+++ src/main/kotlin/io/polygon/kotlin/sdk/rest/reference/Markets.kt
@@ -6,18 +6,21 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /** See [PolygonReferenceClient.getSupportedMarketsBlocking] */
+@Deprecated("This API is no longer supported and will be sunset some time in the future")
 suspend fun PolygonReferenceClient.getSupportedMarkets(vararg opts: PolygonRestOption): MarketsDTO =
     polygonClient.fetchResult({
         path("v2", "reference", "markets")
     }, *opts)
 
 @Serializable
+@Deprecated("used in deprecated getSupportedMarkets")
 data class MarketsDTO(
     val status: String? = null,
     val results: List<MarketDescriptionDTO> = emptyList()
 )
 
 @Serializable
+@Deprecated("used in deprecated getSupportedMarkets")
 data class MarketDescriptionDTO(
     val market: String? = null,
     @SerialName("desc") val description: String? = null
