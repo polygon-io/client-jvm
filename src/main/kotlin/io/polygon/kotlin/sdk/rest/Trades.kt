a5363a876e64395912492db23a62de3f207e53aa
Hunter <6395201+HunterL@users.noreply.github.com>
2023-11-03 17:36:15 -0400

    Update src/main/kotlin/io/polygon/kotlin/sdk/rest/Trades.kt
    Co-authored-by: Michael Moghaddam <6424916+mmoghaddam385@users.noreply.github.com>

--- src/main/kotlin/io/polygon/kotlin/sdk/rest/Trades.kt
+++ src/main/kotlin/io/polygon/kotlin/sdk/rest/Trades.kt
@@ -56,7 +56,7 @@ data class TradesResponse(
     val status: String? = null,
     @SerialName("request_id") val requestID: String? = null,
     @SerialName("next_url") override val nextUrl: String? = null,
-    override val results: List<Trade>? = emptyList()
+    override val results: List<Trade> = emptyList()
 ) : Paginatable<Trade>
 
 
